bd86fd8368518c936e094eb132bec8747104ceda
cesc <cscherhoff@web.de>
2022-04-02 23:16:44 +0200

    Change name of the category table to avoid name conflict with expense-service

--- src/main/java/com/exxeta/allocatorservice/entities/Category.java
+++ src/main/java/com/exxeta/allocatorservice/entities/Category.java
@@ -1,12 +1,10 @@
 package com.exxeta.allocatorservice.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
+@Table(name = "allocation_category")
 public class Category {
 
     @Id
